80853f96da418d7d33177b27fb69a5f731cf914a
박용빈 <qkrdydqls999@gmail.com>
2025-06-25 14:18:26 +0900

    - Book 도메인 관련 코드 구현(6개추가, 1개제거) - ErrorCode 최신화

--- src/main/java/org/example/bookmarket/book/entity/Book.java
+++ src/main/java/org/example/bookmarket/book/entity/Book.java
@@ -1,10 +1,10 @@
 package org.example.bookmarket.book.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
+@Table(name = "books")
 @Getter
 @Setter
 @NoArgsConstructor
@@ -13,5 +13,28 @@ import lombok.*;
 public class Book {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, unique = true, length = 20)
+    private String isbn;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String author;
+
+    @Column(nullable = false, length = 100)
+    private String publisher;
+
+    private Integer publicationYear;
+
+    private Integer newPrice;
+
+    @Lob
+    private String description;
+
+    @Column(length = 255)
+    private String coverImageUrl;
 }
