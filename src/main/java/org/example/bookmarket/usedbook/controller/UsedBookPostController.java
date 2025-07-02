8e47461beea65dbda078e568bd6051101fe8da4b
박용빈 <qkrdydqls999@gmail.com>
2025-07-02 10:24:22 +0900

    21-5

---
+++ src/main/java/org/example/bookmarket/usedbook/controller/UsedBookPostController.java
@@ -0,0 +1,24 @@
+package org.example.bookmarket.usedbook.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.example.bookmarket.usedbook.dto.UsedBookPostRequest;
+import org.example.bookmarket.usedbook.service.UsedBookPostService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/used-books")
+@RequiredArgsConstructor
+public class UsedBookPostController {
+
+    private final UsedBookPostService postService;
+
+    @PostMapping
+    public ResponseEntity<String> registerUsedBook(@ModelAttribute UsedBookPostRequest request) {
+        postService.registerUsedBook(request);
+        return ResponseEntity.ok("중고도서가 등록되었습니다.");
+    }
+}
