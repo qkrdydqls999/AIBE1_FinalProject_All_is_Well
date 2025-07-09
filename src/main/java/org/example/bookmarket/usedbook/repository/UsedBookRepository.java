26c18d63f7d3770d06f86dc93c140c219e092cdd
박용빈 <qkrdydqls999@gmail.com>
2025-07-09 15:41:47 +0900

    - 메인 페이지의 '방금 올라온 따끈한 책' 항목이 실제 usedbook db의 최신 목록을 조회/출력 하도록 수정

--- src/main/java/org/example/bookmarket/usedbook/repository/UsedBookRepository.java
+++ src/main/java/org/example/bookmarket/usedbook/repository/UsedBookRepository.java
@@ -8,4 +8,9 @@ import java.util.List;
 // JpaRepository와 함께 UsedBookRepositoryCustom 인터페이스를 상속받도록 수정
 public interface UsedBookRepository extends JpaRepository<UsedBook, Long>, UsedBookRepositoryCustom {
     List<UsedBook> findBySellerId(Long sellerId);
+
+    /**
+     * 중고책을 ID 역순으로 최대 4건 조회합니다.
+     */
+    List<UsedBook> findTop4ByOrderByIdDesc();
 }
--- src/main/java/org/example/bookmarket/usedbook/service/UsedBookQueryService.java
+++ src/main/java/org/example/bookmarket/usedbook/service/UsedBookQueryService.java
@@ -13,4 +13,12 @@ public interface UsedBookQueryService {
      * [신규 기능 추가] 키워드 리스트로 중고책을 검색하는 메소드
      */
     List<UsedBookResponse> getUsedBooksByKeywords(List<String> keywords);
+
+    /**
+     * 최신 등록된 중고책을 조회합니다.
+     *
+     * @param limit 최대 조회 건수
+     * @return 최신 중고책 목록
+     */
+    List<UsedBookResponse> getLatestUsedBooks(int limit);
 }
